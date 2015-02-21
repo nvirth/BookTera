3a97fc4e2fea2a7e347a634685c0c8930ddc5548
norbert.virth <norbert.virth@gmail.com>
2015-02-21 21:06:42 +0100

    Implemented ProductGroupManagerClient.search(...) fn
    * The whole class (+interface) were created now
    * Updated the mock data with it

--- Helpers/DownloadMockData.bat
+++ Helpers/DownloadMockData.bat
@@ -1,2 +1,5 @@
-wget -O productmanager_getnewests.json "http://localhost:50135/EntityManagers/ProductManagerService.svc/GetNewests?numOfProducts=16&pageNumber=2&productsPerPage=8"
-wget -O productmanager_getmainhighlighteds.json "http://localhost:50135/EntityManagers/ProductManagerService.svc/GetMainHighlighteds?pageNumber=1&productsPerPage=8"
+echo "Downloads the mock data for the AndroidClientProxy project. "
+
+wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productmanager_getnewests.json "http://localhost:50135/EntityManagers/ProductManagerService.svc/GetNewests?numOfProducts=16&pageNumber=2&productsPerPage=8"
+wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productmanager_getmainhighlighteds.json "http://localhost:50135/EntityManagers/ProductManagerService.svc/GetMainHighlighteds?pageNumber=1&productsPerPage=8"
+wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productgroupmanager_search.json "http://localhost:50135/EntityManagers/ProductGroupManagerService.svc/Search?needCategory=false&pageNumber=1&searchText=fen&productsPerPage=100"
