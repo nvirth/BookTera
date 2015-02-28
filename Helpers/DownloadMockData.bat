69d6f3122c3de7e4e744d1212eedbf72f7bd4b8c
norbert.virth <norbert.virth@gmail.com>
2015-02-28 18:57:06 +0100

    Added mock data for AuthenticationClient.loginAndGetId(...)
    Also updated .gitignore files

--- Helpers/DownloadMockData.bat
+++ Helpers/DownloadMockData.bat
@@ -1,5 +1,6 @@
 echo "Downloads the mock data for the AndroidClientProxy project. "
 
+REM --- UnAuth ---
 wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productmanager_getnewests.json "http://localhost:50135/EntityManagers/ProductManagerService.svc/GetNewests?numOfProducts=16&pageNumber=2&productsPerPage=8"
 wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productmanager_getmainhighlighteds.json "http://localhost:50135/EntityManagers/ProductManagerService.svc/GetMainHighlighteds?pageNumber=1&productsPerPage=8"
 wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productgroupmanager_search.json "http://localhost:50135/EntityManagers/ProductGroupManagerService.svc/Search?needCategory=false&pageNumber=1&searchText=fen&productsPerPage=100"
@@ -8,4 +9,13 @@ wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productmanager_getpr
 wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productmanager_getusersproductsbyfriendlyurl.json "http://localhost:50135/EntityManagers/ProductManagerService.svc/GetUsersProductsByFriendlyUrl?forExchange=false&pageNumber=1&friendlyUrl=Boldano&productsPerPage=100"
 wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\productgroupmanager_getfulldetailed.json "http://localhost:50135/EntityManagers/ProductGroupManagerService.svc/GetFullDetailed?pageNumber=1&friendlyUrl=Emma-lanya&productsPerPage=100"
 
-
+REM --- Auth ---
+wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\bookteraauthentication_loginandgetid.json ^
+     --save-cookies Temp\cookies.txt ^
+     "http://localhost:50135/Authentication/BookteraAuthenticationService.svc/LoginAndGetId?userName=zomidudu&password=asdqwe123&persistent=true"
+REM TEST
+GOTO COMMENT
+wget -O ..\Java\Android\AndroidClientProxy\src\main\res\raw\tmp.json ^
+     --load-cookies Temp\cookies.txt ^
+     "http://localhost:50135/EntityManagers/UserProfileManagerService.svc/GetForEdit"
+:COMMENT
